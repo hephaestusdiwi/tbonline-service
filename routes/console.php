43cbd0d9bfb9945d15f750d03107a65db27e7e0c
Apollondiwi <ilhadiwibusiness@gmail.com>
2026-06-07 15:26:48 +0700

    feat: masih fix live chat

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('chat:close-idle')->everyMinute();
+Schedule::command('chat:mark-inactive-visitors')->everyMinute();
